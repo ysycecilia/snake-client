11a66d7b654c023f3cc02d47714e805bda829fb9
Cecilia <ysycecilia@gmail.com>
2021-06-02 23:52:45 +0000

    seperate connect() to client.js

--- play.js
+++ play.js
@@ -1,21 +1,6 @@
-const net = require('net');
+const connect = require("./client");
 
-// establishes a connection with the game server
-const connect = function(data) {
-  const conn = net.createConnection({
-    port: 50542, // IP address here,
-    host: '135.23.222.131'// PORT number here,
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  
-  conn.on('data', (data) => { //get message back from server
-    console.log('Message from server: ', data);
-  });
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
