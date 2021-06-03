9c3c815834a99ee898d1888c1029788f5b74ef52
Cecilia <ysycecilia@gmail.com>
2021-06-03 01:25:51 +0000

    refactor by importing modules and pass returned data from connect() to setupInput()

--- play.js
+++ play.js
@@ -1,7 +1,6 @@
-const connect = require("./client");
-
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 
 console.log("Connecting ...");
-connect();
-
+setupInput(connect());
