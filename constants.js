f5ec0e916b3a2ba462f90a8c32616cc66709238f
Cecilia <ysycecilia@gmail.com>
2021-06-03 01:20:24 +0000

    wrap all static data in a seperate file

---
+++ constants.js
@@ -0,0 +1,17 @@
+const IP = '135.23.222.131';
+const PORT = 50542;
+const MoveUp = "w";
+const MoveRight = "d";
+const MoveDown = "s";
+const MoveLeft = "a";
+const Greeting = "m";
+
+module.exports = {
+  IP,
+  PORT,
+  MoveUp,
+  MoveDown,
+  MoveRight,
+  MoveLeft,
+  Greeting
+};
